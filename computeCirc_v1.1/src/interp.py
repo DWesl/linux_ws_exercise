020194210f58a2136d87fe4f92216526ea50708a
DWesl <22566757+DWesl@users.noreply.github.com>
2022-09-30 16:20:20 -0400

    PERF: Use numexpr for the interpolation.
    This brings a factor-of-three improvement,
    likely from eliminating copies and only doing one pass through inputs,
    since NumPy's started adding SIMD instructions to its UFunc loops.
    
    This is now basically as fast as it needs to be,
    although still an order of magnitude slower than Fortran.

--- computeCirc_v1.1/src/interp.py
+++ computeCirc_v1.1/src/interp.py
@@ -5,7 +5,7 @@ See Also
 scipy.interpolate.interpn
     Similar functionality on irregularly-spaced grids
 """
-
+import numexpr as ne
 import numpy as np
 import numpy.typing as npt
 
@@ -51,24 +51,35 @@ def interp2(
     array([[ 0.,  1.,  2.,  3.],
            [ 4.,  5.,  6.,  7.],
            [ 8.,  9., 10., 11.]])
-
+    >>> interp2(x_grid[:-1, :-1] + 0.5, y_grid[:-1, :-1] + 0.5, 0, 0, 1, 1, arr)
+    array([[2.5, 3.5, 4.5],
+           [6.5, 7.5, 8.5]])
     """
-    idx_float = (x - x0) / dx
+    idx_float = ne.evaluate("(x - x0) / dx")
     left_idx = np.floor(idx_float)
     right_idx = np.ceil(idx_float).astype(int)
     x_remainder = idx_float - left_idx
     left_idx = left_idx.astype(int)
 
-    idx_float = (y - y0) / dy
+    idx_float = ne.evaluate("(y - y0) / dy")
     bottom_idx = np.floor(idx_float)
     top_idx = np.ceil(idx_float).astype(int)
     y_remainder = idx_float - bottom_idx
     bottom_idx = bottom_idx.astype(int)
 
-    result = (
-        var[bottom_idx, left_idx] * (1 - y_remainder) * (1 - x_remainder)
-        + var[bottom_idx, right_idx] * (1 - y_remainder) * x_remainder
-        + var[top_idx, left_idx] * y_remainder * (1 - x_remainder)
-        + var[top_idx, right_idx] * y_remainder * x_remainder
+    result = ne.evaluate(
+        "var_bl * (1 - y_fpart) * (1 - x_fpart)"
+        "+ var_br * (1 - y_fpart) * x_fpart"
+        "+ var_tl * y_fpart * (1 - x_fpart)"
+        "+ var_tr * y_fpart * x_fpart",
+        local_dict={
+            "var_bl": var[bottom_idx, left_idx],
+            "var_br": var[bottom_idx, right_idx],
+            "var_tl": var[top_idx, left_idx],
+            "var_tr": var[top_idx, right_idx],
+            "y_fpart": y_remainder,
+            "x_fpart": x_remainder,
+        },
     )
+
     return result
